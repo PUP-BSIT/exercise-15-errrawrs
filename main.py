2421c42a7b24319ca95e3716b1ecc2359fc1c40d
victorioso_daniel <danielvictorioso.acd@gmail.com>
2024-12-18 13:01:31 +0800

    add my module

---
+++ main.py
@@ -0,0 +1,11 @@
+from errawrs.victorioso import InventoryManager
+
+def main_menu():
+    print("1. Ivan Delumen")
+    print("2. Gerald Mamasalanang")
+    print("3. Michael Angelo Mosquito")
+    print("4. Simone Jake Reyes")
+    print("5. Daniel Victorioso")
+
+manager = InventoryManager()
+manager.display_menu()
